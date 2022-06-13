675817f185fc67f905102774699841079a13f24b
Shaun <772542545@qq.com>
2022-06-13 19:06:11 +0800

    .

--- test002/src/main.rs
+++ test002/src/main.rs
@@ -6,7 +6,6 @@ use flume::{unbounded, Receiver, Sender};
 use std::collections::HashMap;
 mod apple;
 use apple::conf::ipadd;
-use bytes::{BytesMut, BufMut};
 use serde::{Deserialize, Serialize};
 lazy_static! {
     // static ref S_CMSG:(Sender<cmsg::CMsg>, Receiver<cmsg::CMsg>) = unbounded();
@@ -16,15 +15,15 @@ lazy_static! {
 
 #[tokio::main]
  async fn main() {
-    let sock = UdpSocket::bind("0.0.0.0:8080".parse::<SocketAddr>().unwrap()).await.unwrap();
+    let sock = UdpSocket::bind(ipadd::Conf::local_server().parse::<SocketAddr>().unwrap()).await.unwrap();
     let field_name = String::from("type");
     let field_value = String::from("IP");
-
     let mut map = HashMap::new();
-
     map.insert(field_name, field_value);
 
     let buf = serde_json::to_vec(&map).unwrap();
+
+
 
     let addr = ipadd::Conf::remote_server();
 
@@ -36,13 +35,6 @@ lazy_static! {
     println!("{:?} bytes received from {:?}", len, addr);
     let obj:HashMap<String,String> = serde_json::from_slice(&buf2[..len]).unwrap();
     println!("obj is {:?}",obj);
-
-
-    // let buf2 = String::from_utf8_lossy(&buf2);
-    // let buf2 = buf2.trim_matches(char::from(0));
-    // println!("{:?}",buf2);
-    // let obj:HashMap<String,String> = serde_json::from_str(buf2).unwrap();
-    // println!("{:?}",obj);
 
     match obj.get("object") {
         Some(rst)=>{
