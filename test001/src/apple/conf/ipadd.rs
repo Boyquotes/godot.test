6bffe56d908625887298a57a7de5d84be9d736df
Shaun <772542545@qq.com>
2022-06-20 10:49:26 +0800

    .

--- test001/src/apple/conf/ipadd.rs
+++ test001/src/apple/conf/ipadd.rs
@@ -1,15 +1,7 @@
-// use std::path::{PathBuf,Path};
-// use configparser::ini::Ini;
-
-
 pub struct URL;
 impl URL {
 
     pub fn remote_server() -> String {
-        // let mut config = Ini::new();
-        // config.load(PathBuf::from(Path::new(".")).join("conf").join("ipadd.ini")).unwrap();
-        // let host = config.get("remote_server", "host").unwrap_or("127.0.0.1".to_owned());
-        // let port = config.get("remote_server", "port").unwrap_or("0".to_owned());
         let host = "118.193.46.124";
         let port = 5016;
         format!("{}:{}", host, port)
@@ -17,10 +9,6 @@ impl URL {
     }
 
     pub fn local_server() -> String {
-        // let mut config = Ini::new();
-        // config.load(PathBuf::from(Path::new(".")).join("conf").join("ipadd.ini")).unwrap();
-        // let host = config.get("local_server", "host").unwrap_or("127.0.0.1".to_owned());
-        // let port = config.get("local_server", "port").unwrap_or("0".to_owned());
         let host = "0.0.0.0";
         let port = 5016;
         format!("{}:{}", host, port)
